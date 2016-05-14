1a02ccb4f38cd8839343ccc8ab247981b76d8b42
Todd Dabney <todd.dabney@gmail.com>
2016-05-14 13:12:04 -0700

    message test

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -1,7 +1,14 @@
 class MessengerController < Messenger::MessengerController
   def webhook
-    puts 'test'
     render nothing: true, status: 200
     Item.create(owner: 1)
   end
+  def test_message
+    Messenger::Client.send(
+	Messenger::Request.new(
+		Messenger::Elements::Text.new(text: 'some text'),
+      		fb_params.first_entry.sender_id
+    )
+  )
+      render nothing: true status: 200
 end
