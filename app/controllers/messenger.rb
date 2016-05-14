16768d3e0fd229beda771e170c741340da292d7e
Todd Dabney <todd.dabney@gmail.com>
2016-05-14 13:19:24 -0700

    fixing bug

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -3,13 +3,4 @@ class MessengerController < Messenger::MessengerController
     render nothing: true, status: 200
     Item.create(owner: 1)
   end
-  def test_message
-    Messenger::Client.send(
-	Messenger::Request.new(
-		Messenger::Elements::Text.new(text: 'some text'),
-      		fb_params.first_entry.sender_id
-			    )
-			)
-      render nothing: true status: 200
-      end
 end
