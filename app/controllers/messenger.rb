1cb36bb9445787e8262d8633dfcb3cd0d5fb0070
Todd Dabney <todd.dabney@gmail.com>
2016-05-14 19:46:35 -0700

    testing

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -1,7 +1,8 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     render nothing: true, status: 200
-    puts params
+    puts fb_params.entries.each do |entry|
+      puts entry
     #Item.parse_message()
   end
 end
