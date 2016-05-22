e206c740edce18770a68a02b886dd77df9ebf1cf
Ethan Barhydt <ethanbarhydt@gmail.com>
2016-05-22 14:24:55 -0700

    Add sender and write back a message

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,12 @@ class Item < ActiveRecord::Base
       item = msg.split(" is ")
       Item.create(key: item[0], value: item[1], owner: owner)
       puts Item.all
+      Messenger::Client.send(
+        Messenger::Request.new(
+          Messenger::Elements::Text.new(text: "saved!"),
+          owner
+        )
+      )
     else
       item = Item.find_by(key: msg)
       Messenger::Client.send(
