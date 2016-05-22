0b9b81e6a0426b541189456c966c835486140cbe
Ethan Barhydt <ethanbarhydt@gmail.com>
2016-05-22 11:31:45 -0700

    Try printing reg params

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -1,8 +1,8 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     render nothing: true, status: 200
-    # @message = params["entry"].first["messaging"].first["message"]["text"]
-    # puts @message
+    @message = params["entry"].first["messaging"].first["message"]["text"]
+    puts @message
     #AWAITING STRUCTURE OF PARAMS
     puts "facebook params are " + fb_params.to_s
   end
