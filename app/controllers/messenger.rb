f6d2fbd3c9eb2f758d9e0fb667e4d30fcc0b8cf2
Ethan Barhydt <ethanbarhydt@gmail.com>
2016-05-22 14:10:30 -0700

    error handling

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -1,14 +1,38 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     render nothing: true, status: 200
-    if fb_params.first_entry.callback.message?
-      @message = fb_params.first_entry.callback.message
-      puts @message
-      @owner = fb_params.first_entry.sender_id
-      puts @owner
-      Item.parse_message(@message, @owner)
-    else
-      puts "no message"
+    if params.key?("entry") && params["entry"].any?
+      entry = params["entry"].first
+      puts "entry"
+      if entry.key?("messaging") && entry["messaging"].any?
+        messaging = entry["messaging"].first
+        puts "messaging"
+        if messaging.key?("message") && messaging["message"].any?
+          message = messaging["message"]
+          puts "message"
+          if message.key?("text")
+            @message = message["text"]
+          end
+        end
+      end
     end
+    if params.key?("entry") && params["entry"].any?
+      entry = params["entry"].first
+      puts "entry"
+      if entry.key?("messaging") && entry["messaging"].any?
+        messaging = entry["messaging"].first
+        puts "messaging"
+        if messaging.key?("sender") && messaging["sender"].any?
+          sender = messaging["sender"]
+          puts "sender"
+          if sender.key?("id")
+            @sender = sender["id"]
+          end
+        end
+      end
+    end
+    puts @message
+    puts @owner
+    Item.parse_message(@message, @owner)
   end
 end
