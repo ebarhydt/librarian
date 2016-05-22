38f539123ee42fef9632e51dc7260c57c9c15f96
Ethan Barhydt <ethanbarhydt@gmail.com>
2016-05-22 11:27:20 -0700

    parse params

--- app/controllers/messenger.rb
+++ app/controllers/messenger.rb
@@ -1,6 +1,8 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     render nothing: true, status: 200
+    @message = params["entry"].first["messaging"].first["message"]["text"]
+    puts @message
     #AWAITING STRUCTURE OF PARAMS
     #Item.parse_message()
   end
